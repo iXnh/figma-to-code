d4ec2c262f464799c8cf2160654325fe10af1549
ikhsannh <ikhsannurhuda25@gmail.com>
2022-10-25 21:57:21 +0700

    update table version

---
+++ class-chess/script.js
@@ -0,0 +1,10 @@
+//start nav scrolled
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 60){
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
