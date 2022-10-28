969b169e0e2364ff6e155fb487bd6bed68480907
ikhsannh <ikhsannurhuda25@gmail.com>
2022-10-28 16:28:02 +0700

    update media query phone version

--- class-chess/script.js
+++ class-chess/script.js
@@ -1,10 +1,13 @@
 //start nav scrolled
-const nav = document.querySelector("nav");
+const nav       = document.querySelector("nav");
+const mobileNav = document.querySelector("nav.mobile-nav");
 
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 60){
         nav.classList.add("scrolled");
+        mobileNav.classList.add("scrolled");
     } else {
         nav.classList.remove("scrolled");
+        mobileNav.classList.remove("scrolled");
     }
 });
